a41530a860385fd1adbe652008dadff93d6d4609
scalzdp <275735763@qq.com>
2019-04-14 16:04:47 +0800

    修改eureka集成服务

--- src/main/java/com/dapeng/demo/DemoApplication.java
+++ src/main/java/com/dapeng/demo/DemoApplication.java
@@ -2,18 +2,31 @@ package com.dapeng.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient //本服务启动后会自动注册进Eureka服务中
-//@EnableDiscoveryClient //服务发现
-@SpringBootApplication
 @RestController
+@EnableEurekaClient
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    /**
+     * 定义一个简单接口
+     * @param name
+     * @return
+     */
+    @GetMapping("/hi/{name}")
+    public String home(@PathVariable String name){
+        return "hi " + name + ",I am from port :" ;
     }
 
 }
